f9548bd41e2339147d68ce68cc319c9981d9cc63
Dmitry Ermakov <dmitryermakov@MacBook-Pro-Dmitry.local>
2023-12-25 22:34:23 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let myName = 'Dmitry'
+let myAge = '32'
+let myCity = 'Voronezh'
+let myHobby = 'Work, Gym, Webflow and Photo'
+
+console.log('Hello!')
+console.log('My name is', myName)
+console.log("I'm", myAge, "years old")
+console.log("I from", myCity)
+console.log('I like', myHobby)
