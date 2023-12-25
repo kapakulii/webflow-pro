04b1eaf8be9e4e6595405364bcf883d8cf6b46c2
Dmitry <kapakulii@gmail.com>
2023-12-25 23:31:05 +0300

    2.3

--- 2.3/main.js
+++ 2.3/main.js
@@ -2,5 +2,5 @@ let titleProject = 'Оценка стоимости';
 let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
 let screenPrice = 500;
 let percentage = 15;
-let fullPrice;
+let fullPrice = 5000;
 let responsive = true;
